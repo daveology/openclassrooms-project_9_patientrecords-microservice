65350918761922039899d66e927eace6f39f53ce
David <cheery.spot1750@fastmail.com>
2021-12-23 20:52:43 +0100

    build: add Spring Boot project base structure

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # openclassrooms_9th-project_mediscreen
+
 Detect risk factors for disease using predictive analysis of patient populations.
---
+++ src/main/java/com/example/openclassrooms_9thproject_mediscreen/Openclassrooms9thProjectMediscreenApplication.java
@@ -0,0 +1,13 @@
+package com.example.openclassrooms_9thproject_mediscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Openclassrooms9thProjectMediscreenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Openclassrooms9thProjectMediscreenApplication.class, args);
+    }
+
+}
