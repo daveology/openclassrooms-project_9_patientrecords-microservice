33be1ac7e1ded53083e8316addcca39902482813
David <cheery.spot1750@fastmail.com>
2021-12-27 16:34:56 +0100

    build: add first and last name attributes to Patient

--- src/main/java/com/mediscreen/model/Patient.java
+++ src/main/java/com/mediscreen/model/Patient.java
@@ -1,6 +1,8 @@
 package com.mediscreen.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "patient")
@@ -10,4 +12,14 @@ public class Patient {
     @GeneratedValue(strategy= GenerationType.AUTO)
     @Column(name = "patient_id")
     private Long patientId;
+
+    @NotBlank(message = "Username is mandatory")
+    @Size(max=100)
+    @Column(name = "first_name")
+    private String firstName;
+
+    @NotBlank(message = "Username is mandatory")
+    @Size(max=100)
+    @Column(name = "last_name")
+    private String lastName;
 }
