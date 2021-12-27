c0c44eb0d850037df33d8dcbed8bff40938069d2
David <cheery.spot1750@fastmail.com>
2021-12-27 20:43:18 +0100

    test: instanciate PatientRepository to PatientTests

--- src/test/java/com/mediscreen/unit/PatientTests.java
+++ src/test/java/com/mediscreen/unit/PatientTests.java
@@ -1,9 +1,28 @@
 package com.mediscreen.unit;
 
+import com.mediscreen.model.Patient;
+import com.mediscreen.repository.PatientRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 class PatientTests {
 
+    @Autowired
+    private PatientRepository patientRepository;
 
+    @Test
+    public void patientTest() {
+
+        Patient patient = new Patient();
+        patient.setFirstName("Harry");
+        patient.setLastName("Potter");
+        patient.setGender("M");
+        patient.setBirthDate(LocalDate.now().minusYears(12));
+        patient.setAddress("4, Privet Drive, Little Whinging");
+        patient.setPhone("791-112-3456");
+    }
 }
