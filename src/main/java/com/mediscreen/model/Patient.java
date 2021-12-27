c16bc86d9227d7c17ccf6cc375724aa8242c31e4
David <cheery.spot1750@fastmail.com>
2021-12-27 21:23:16 +0100

    build: update id generation from AUTO to IDENTITY

--- src/main/java/com/mediscreen/model/Patient.java
+++ src/main/java/com/mediscreen/model/Patient.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 public class Patient {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name = "patient_id")
     private Long patientId;
 
