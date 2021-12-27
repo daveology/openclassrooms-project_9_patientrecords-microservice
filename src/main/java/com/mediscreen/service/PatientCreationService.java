bb1bec02116d7eca3d1034b6821f89a8f1c073e9
David <cheery.spot1750@fastmail.com>
2021-12-27 22:00:49 +0100

    feat: add createPatient to PatientCreationService

--- src/main/java/com/mediscreen/service/PatientCreationService.java
+++ src/main/java/com/mediscreen/service/PatientCreationService.java
@@ -1,5 +1,6 @@
 package com.mediscreen.service;
 
+import com.mediscreen.model.Patient;
 import com.mediscreen.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,9 @@ public class PatientCreationService {
 
     @Autowired
     private PatientRepository patientRepository;
+
+    public Patient createPatient(Patient patient) {
+
+        return patientRepository.save(patient);
+    }
 }
