adb9d3dc4fc7f452df01075802c5a30de21b6bd4
David <cheery.spot1750@fastmail.com>
2021-12-27 10:38:46 +0100

    refacto: rename package to com.mediscreen

--- src/main/java/com/mediscreen/MediscreenApplication.java
+++ src/main/java/com/mediscreen/MediscreenApplication.java
@@ -1,4 +1,4 @@
-package com.example.openclassrooms_9thproject_mediscreen;
+package com.mediscreen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/mediscreen/MediscreenApplicationTests.java
+++ src/test/java/com/mediscreen/MediscreenApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.openclassrooms_9thproject_mediscreen;
+package com.mediscreen;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
