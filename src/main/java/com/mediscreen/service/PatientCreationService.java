409aa59110108ec2ca3836473cd57e2b6a83692d
David <cheery.spot1750@fastmail.com>
2021-12-27 22:00:23 +0100

    feat: instanciate PatientRepository into PatientCreationService

--- src/main/java/com/mediscreen/service/PatientCreationService.java
+++ src/main/java/com/mediscreen/service/PatientCreationService.java
@@ -1,8 +1,12 @@
 package com.mediscreen.service;
 
+import com.mediscreen.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PatientCreationService {
 
+    @Autowired
+    private PatientRepository patientRepository;
 }
