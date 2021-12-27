2c34f19cf02ff6ef88f39bc7f986ff2ae01e5a7f
David <cheery.spot1750@fastmail.com>
2021-12-27 10:41:20 +0100

    refacto: update spacing

--- src/main/java/com/mediscreen/MediscreenApplication.java
+++ src/main/java/com/mediscreen/MediscreenApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MediscreenApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MediscreenApplication.class, args);
     }
-
 }
