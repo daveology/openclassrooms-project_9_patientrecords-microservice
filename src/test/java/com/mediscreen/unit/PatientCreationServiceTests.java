ee17659b3d0679652136b47bf593ebcb42ecb333
David <cheery.spot1750@fastmail.com>
2021-12-27 22:03:52 +0100

    feat: instanciate patientCreationService into PatientCreationServiceTests

--- src/test/java/com/mediscreen/unit/PatientCreationServiceTests.java
+++ src/test/java/com/mediscreen/unit/PatientCreationServiceTests.java
@@ -1,9 +1,12 @@
 package com.mediscreen.unit;
 
+import com.mediscreen.service.PatientCreationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class PatientCreationServiceTests {
 
-
+    @Autowired
+    PatientCreationService patientCreationService;
 }
