2966fa7c8989af7b9b6d71084c2117f64222f0ec
David <cheery.spot1750@fastmail.com>
2021-12-28 16:04:46 +0100

    feat: update addPatient

--- src/main/java/com/mediscreen/controller/PatientController.java
+++ src/main/java/com/mediscreen/controller/PatientController.java
@@ -1,9 +1,10 @@
 package com.mediscreen.controller;
 
+import com.mediscreen.model.Patient;
 import com.mediscreen.service.PatientCreationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +13,9 @@ public class PatientController {
     @Autowired
     PatientCreationService patientCreationService;
 
-    @GetMapping("/patient/add")
-    public String addPatient(Model model) {
+    @PostMapping("/patient/add")
+    public Patient addPatient(@RequestBody Patient patient) {
 
-        return "patient/add";
+        return patientCreationService.createPatient(patient);
     }
 }
