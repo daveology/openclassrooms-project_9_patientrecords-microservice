b80644c508839d9118e100e470730efee8dfd4b5
David <cheery.spot1750@fastmail.com>
2021-12-28 22:21:29 +0100

    feat: instanciate PatientRepository into PatientDeleteService

--- src/main/java/com/mediscreen/service/PatientDeleteService.java
+++ src/main/java/com/mediscreen/service/PatientDeleteService.java
@@ -1,9 +1,12 @@
 package com.mediscreen.service;
 
+import com.mediscreen.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PatientDeleteService {
 
-
+    @Autowired
+    private PatientRepository patientRepository;
 }
