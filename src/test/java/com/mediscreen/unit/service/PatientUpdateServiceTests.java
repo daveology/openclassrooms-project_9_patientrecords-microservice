37c80d84a29b82e4052ff298d1f68f82ce63c146
David <cheery.spot1750@fastmail.com>
2021-12-28 21:46:41 +0100

    test: instanciate PatientUpdateService in PatientUpdateServiceTests

--- src/test/java/com/mediscreen/unit/service/PatientUpdateServiceTests.java
+++ src/test/java/com/mediscreen/unit/service/PatientUpdateServiceTests.java
@@ -1,9 +1,12 @@
 package com.mediscreen.unit.service;
 
+import com.mediscreen.service.PatientUpdateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class PatientUpdateServiceTests {
 
-
+    @Autowired
+    private PatientUpdateService patientUpdateService;
 }
