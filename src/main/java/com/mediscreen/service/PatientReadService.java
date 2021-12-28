8d6c40793f90cd7ddfeebb67058c4ed53d93e3f9
David <cheery.spot1750@fastmail.com>
2021-12-28 16:39:23 +0100

    feat: add readPatientById service method

--- src/main/java/com/mediscreen/service/PatientReadService.java
+++ src/main/java/com/mediscreen/service/PatientReadService.java
@@ -1,5 +1,6 @@
 package com.mediscreen.service;
 
+import com.mediscreen.model.Patient;
 import com.mediscreen.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,9 @@ public class PatientReadService {
 
     @Autowired
     private PatientRepository patientRepository;
+
+    public Patient readPatientById(Long patientId) {
+
+        return patientRepository.getById(patientId);
+    }
 }
