449b9ccf4e6f5d06cd57fdcd063b3c347e90b652
David <cheery.spot1750@fastmail.com>
2021-12-28 16:55:25 +0100

    test: add shouldGetPatientById service unit test

--- src/test/java/com/mediscreen/unit/service/PatientReadServiceTests.java
+++ src/test/java/com/mediscreen/unit/service/PatientReadServiceTests.java
@@ -1,10 +1,15 @@
 package com.mediscreen.unit.service;
 
+import com.mediscreen.model.Patient;
 import com.mediscreen.repository.PatientRepository;
-import com.mediscreen.service.PatientCreationService;
 import com.mediscreen.service.PatientReadService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class PatientReadServiceTests {
@@ -13,4 +18,15 @@ public class PatientReadServiceTests {
     PatientReadService patientReadService;
     @Autowired
     PatientRepository patientRepository;
+
+    @Test
+    public void shouldGetPatientById() {
+
+        Patient patient = patientRepository.save(new Patient("Ron", "WEASLEY", "M", LocalDate.now().minusYears(12),
+                "The Borrow, Ottery St. Catchpole", "791-145-6752"));
+        Long patientId = patient.getPatientId();
+        Patient actualPatient = patientReadService.readPatientById(patientId);
+
+        assertEquals(patient, actualPatient);
+    }
 }
