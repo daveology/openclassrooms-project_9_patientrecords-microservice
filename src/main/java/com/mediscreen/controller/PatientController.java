a88a653b5416193780851ff63c9b27d39da87c0c
David <cheery.spot1750@fastmail.com>
2021-12-28 15:46:47 +0100

    feat: add PatientCreationService instanciation into controller

--- src/main/java/com/mediscreen/controller/PatientController.java
+++ src/main/java/com/mediscreen/controller/PatientController.java
@@ -1,4 +1,12 @@
 package com.mediscreen.controller;
 
+import com.mediscreen.service.PatientCreationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PatientController {
+
+    @Autowired
+    PatientCreationService patientCreationService;
 }
