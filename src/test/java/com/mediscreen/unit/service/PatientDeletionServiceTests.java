48f475f191f3f20064e1269754eed797dc249681
David <cheery.spot1750@fastmail.com>
2021-12-28 22:29:33 +0100

    feat: instanciate PatientDeletionService into PatientDeletionServiceTests

--- src/test/java/com/mediscreen/unit/service/PatientDeletionServiceTests.java
+++ src/test/java/com/mediscreen/unit/service/PatientDeletionServiceTests.java
@@ -1,9 +1,12 @@
 package com.mediscreen.unit.service;
 
+import com.mediscreen.service.PatientDeletionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class PatientDeletionServiceTests {
 
-
+    @Autowired
+    private PatientDeletionService patientDeletionService;
 }
