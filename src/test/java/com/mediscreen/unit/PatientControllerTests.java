7ad8f5a7e275a063a3cbbc30097bfedbd3a88058
David <cheery.spot1750@fastmail.com>
2021-12-28 16:14:35 +0100

    test: add shouldCreatePatient endpoint unit test

--- src/test/java/com/mediscreen/unit/PatientControllerTests.java
+++ src/test/java/com/mediscreen/unit/PatientControllerTests.java
@@ -1,10 +1,18 @@
 package com.mediscreen.unit;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mediscreen.model.Patient;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters=false)
@@ -15,5 +23,16 @@ public class PatientControllerTests {
     @Autowired
     private ObjectMapper objectMapper;
 
-    
+    @Test
+    public void shouldCreatePatient() throws Exception {
+
+        Patient patient = new Patient("Harry", "POTTER", "M", LocalDate.now().minusYears(12),
+                "4, Privet Drive, Little Whinging", "791-112-3456");
+
+        mockMvc.perform(post("/patient/add")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(patient))
+                ).andExpect(status().isOk());
+    }
 }
