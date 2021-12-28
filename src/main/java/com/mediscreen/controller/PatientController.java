2a6cc4cb77776518febaadd9b6657b85cb3f0c11
David <cheery.spot1750@fastmail.com>
2021-12-28 15:49:13 +0100

    feat: add patient creation endpoint

--- src/main/java/com/mediscreen/controller/PatientController.java
+++ src/main/java/com/mediscreen/controller/PatientController.java
@@ -2,6 +2,8 @@ package com.mediscreen.controller;
 
 import com.mediscreen.service.PatientCreationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +11,10 @@ public class PatientController {
 
     @Autowired
     PatientCreationService patientCreationService;
+
+    @GetMapping("/patient/add")
+    public String addPatient(Model model) {
+
+
+    }
 }
