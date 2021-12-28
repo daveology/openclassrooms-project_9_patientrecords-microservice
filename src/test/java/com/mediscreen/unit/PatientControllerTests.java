8b4a353eaefbe3c33a4eff5cfcd59d627d4a8a6b
David <cheery.spot1750@fastmail.com>
2021-12-28 16:09:36 +0100

    test: add MockMvc and ObjectMapper into PatientControllerTests

--- src/test/java/com/mediscreen/unit/PatientControllerTests.java
+++ src/test/java/com/mediscreen/unit/PatientControllerTests.java
@@ -1,9 +1,17 @@
 package com.mediscreen.unit;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PatientControllerTests {
 
-    
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 }
