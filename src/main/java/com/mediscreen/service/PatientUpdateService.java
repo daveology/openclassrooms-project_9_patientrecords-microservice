47b0bec97d708923f5c627b43ce855f2150831cc
David <cheery.spot1750@fastmail.com>
2021-12-28 21:41:26 +0100

    feat: add updatePatient service method

--- src/main/java/com/mediscreen/service/PatientUpdateService.java
+++ src/main/java/com/mediscreen/service/PatientUpdateService.java
@@ -1,12 +1,30 @@
 package com.mediscreen.service;
 
+import com.mediscreen.model.Patient;
 import com.mediscreen.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PatientUpdateService {
 
     @Autowired
     private PatientRepository patientRepository;
+
+    public Patient updatePatient(Patient patient) {
+
+        Patient updatedPatient;
+        Optional<Patient> optionalUser = patientRepository.findById(patient.getPatientId());
+
+        if (optionalUser.isPresent()) {
+            updatedPatient = optionalUser.get();
+            patientRepository.save(updatedPatient);
+        } else {
+            return new Patient();
+        }
+
+        return updatedPatient;
+    }
 }
