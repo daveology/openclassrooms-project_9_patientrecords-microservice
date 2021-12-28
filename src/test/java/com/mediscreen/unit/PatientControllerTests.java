9ebeccb4022ec75d2b66021642e68698b4ba65d1
David <cheery.spot1750@fastmail.com>
2021-12-28 16:16:55 +0100

    test: add shouldCreatePatientList endpoint unit test

--- src/test/java/com/mediscreen/unit/PatientControllerTests.java
+++ src/test/java/com/mediscreen/unit/PatientControllerTests.java
@@ -10,6 +10,8 @@ import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequ
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,5 +36,26 @@ public class PatientControllerTests {
                         .contentType("application/json")
                         .content(objectMapper.writeValueAsString(patient))
                 ).andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldCreatePatientList() throws Exception {
+
+        Collection<Patient> patientList = new ArrayList<>();
+        Patient firstPatient = new Patient("Harry", "POTTER", "M", LocalDate.now().minusYears(12),
+                "4, Privet Drive, Little Whinging", "791-112-3456");
+        patientList.add(firstPatient);
+        Patient secondPatient = new Patient("Ron", "WEASLEY", "M", LocalDate.now().minusYears(12),
+                "The Borrow, Ottery St. Catchpole", "791-145-6752");
+        patientList.add(secondPatient);
+        Patient thirdPatient = new Patient("Hermione", "GRANGER", "F", LocalDate.now().minusYears(12),
+                "8 Heathgate, Hampstead Garden Suburb, London", "791-963-4175");
+        patientList.add(thirdPatient);
+
+        mockMvc.perform(post("/patientList/add")
+                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(patientList))
+        ).andExpect(status().isOk());
     }
 }
